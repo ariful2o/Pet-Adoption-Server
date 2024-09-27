598b12a484e3101ff1111682a67e86888ad02012
Mohammad Ariful <arifulz848@gmail.com  git config --global user.name mohammadariful20>
2024-09-27 19:42:47 +0600

    create a new token and clearing the token

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 require("dotenv").config();
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
 const app = express()
+
 
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true,
+}))
+app.use(cookieParser())
 
 //connection mongodb database
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_UserName}:${process.env.DB_Password}@cluster0.0zrlznh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,6 +30,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 
 async function run() {
   try {
@@ -34,19 +44,46 @@ async function run() {
     const catsCollection = database.collection("cats");
 
     // Perform CRUD operations here
-    app.get("/dogs",async (req,res)=>{
+    app.get("/dogs", async (req, res) => {
       const dogs = await dogsCollection.find().toArray();
       res.send(dogs)
     })
-    app.get("/dog/:id", async (req,res)=>{
-      const dog = await dogsCollection.findOne({_id: new ObjectId(req.params.id)})
+    app.get("/dog/:id", async (req, res) => {
+      const dog = await dogsCollection.findOne({ _id: new ObjectId(req.params.id) })
       res.send(dog)
     })
-    app.get("/cats",async (req,res)=>{
+    app.get("/cats", async (req, res) => {
       const cats = await catsCollection.find().toArray();
       res.send(cats)
     })
 
+    // jwt request
+
+    //genate a secret key require('crypto').randomBytes(64).toString('hex')
+    
+    const cookieOptions = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    };
+    //creating Token
+    app.post("/jwt",  async (req, res) => {
+      const user = req.body.email;
+      // console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+      res.cookie("token", token, cookieOptions).send({ success: true});
+    });
+ 
+
+    //clearing Token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      // console.log("logging out", user);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
